6281e2b773574e3c45c0e178b07e79be6be7c074
jotelu <93430466+jotelu@users.noreply.github.com>
2020-11-08 13:43:39 +0000

    Updated vendor/ZendFramework

--- examples/list.rs
+++ examples/list.rs
@@ -2,4 +2,6 @@ pub fn main() {
     smol::block_on(async {
         let http = isahc::HttpClient::new().unwrap();
 
-        let client = coingecko::Client::new(
+        let client = coingecko::Client::new(http);
+
+  
