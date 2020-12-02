4d0a2bf5ecac9cc3debcc742477c4af3ae7e2f69
jotelu <93430466+jotelu@users.noreply.github.com>
2020-12-02 05:40:29 +0000

    passing

--- examples/markets.rs
+++ examples/markets.rs
@@ -2,4 +2,6 @@ use coingecko::{CoinsMarketsReq, MarketsReqOrder};
 
 pub fn main() {
     smol::block_on(async {
-        let http = isahc::HttpClient::new().un
+        let http = isahc::HttpClient::new().unwrap();
+
+    
