15cf803de87b3077130a0d25b035ccca3e40352a
jotelu <93430466+jotelu@users.noreply.github.com>
2020-12-03 12:22:27 +0000

    parser fixes

--- examples/markets.rs
+++ examples/markets.rs
@@ -4,4 +4,6 @@ pub fn main() {
     smol::block_on(async {
         let http = isahc::HttpClient::new().unwrap();
 
-        let client = coingecko::Client::n
+        let client = coingecko::Client::new(http);
+
+   
