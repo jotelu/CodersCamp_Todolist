17763d70448c351246d4e278bda9c3d3138cf2ec
jotelu <93430466+jotelu@users.noreply.github.com>
2021-01-02 20:52:06 +0000

    example data

--- examples/ping.rs
+++ examples/ping.rs
@@ -1,3 +1,10 @@
 
 pub fn main() {
     smol::block_on(async {
+        let http = isahc::HttpClient::new().unwrap();
+
+        let client = coingecko::Client::new(http);
+
+        println!("{:#?}", client.ping().await);
+    })
+}
