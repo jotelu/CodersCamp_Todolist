c91834a3a2a4f692034150512d8f2e877285622c
jotelu <93430466+jotelu@users.noreply.github.com>
2021-01-31 13:35:40 +0000

    ui improvement

--- examples/simple_price.rs
+++ examples/simple_price.rs
@@ -6,4 +6,4 @@ pub fn main() {
 
         let client = Client::new(http);
 
-        let req = SimplePriceReq::new("ethereum,algorand,tezos".into()
+        let req = SimplePriceReq::new("ethereum,algorand,tezos".into(), "usd".into()
