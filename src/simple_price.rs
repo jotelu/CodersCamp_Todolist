a0bb685ecf776cd68e91dd3c2de2c7fd4179afdb
jotelu <93430466+jotelu@users.noreply.github.com>
2022-12-29 22:50:28 +0000

    powerpc/perf: Cap 64bit userspace backtraces to PERF_MAX_STACK_DEPTH

--- src/simple_price.rs
+++ src/simple_price.rs
@@ -40,4 +40,4 @@ impl SimplePriceReq {
         fomat!(
             "ids=" (self.ids)
             "&vs_currencies=" (self.vs_currencies)
-            if (self.inc
+            if (self.include_market_cap) {
