4fee6e54a07abda00f69ab5c6f4cb2d5437ac04e
jotelu <93430466+jotelu@users.noreply.github.com>
2023-04-23 20:03:52 +0000

    unit tests fix

--- src/utils.rs
+++ src/utils.rs
@@ -4,4 +4,5 @@ use isahc::http::Request;
 use isahc::HttpClient;
 use serde::de::DeserializeOwned;
 
-pub async fn get_json<T: DeserializeOwned>(client: &HttpClient, uri: &str) -> Result<T, 
+pub async fn get_json<T: DeserializeOwned>(client: &HttpClient, uri: &str) -> Result<T, Error> {
+    let 
